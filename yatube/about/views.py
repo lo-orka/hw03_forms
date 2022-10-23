845b2b7884388a5a523229e1cde10b80feb1d4f1
Валерия Шакарян <valer.pd.w@gmail.com>
2022-10-23 23:51:26 +0500

    Третий коммит

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -6,6 +6,5 @@ app_name = 'about'
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
 
-
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -5,6 +5,4 @@ def year(request):
     """Добавляет переменную с текущим годом."""
     date = datetime.datetime.now().year
     date = int(date)
-    return {
-       'year': date
-    }
+    return {'year': date}
