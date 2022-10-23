8a8cbcc10b260322f176eab3af8a1f3b9d1880fb
Валерия Шакарян <valer.pd.w@gmail.com>
2022-10-23 23:54:41 +0500

    Четвертый коммит

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -3,8 +3,10 @@ from django.views.generic.base import TemplateView
 
 app_name = 'about'
 
+
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
+
 
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
